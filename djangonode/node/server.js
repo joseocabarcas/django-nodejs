f81792eed7eb2aa01405bc949dcda122e9f7b506
José Cabarcas <josecabarcas94@gmail.com>
2016-03-05 11:15:20 -0500

    install and setting Socketio

---
+++ djangonode/node/server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var server = http.createServer().listen(3000);
+var io = require('socket.io').listen(server);
+var querystring = require('querystring');
+
+io.on('connection', function(socket){
+    console.log("Conectado al socket");
+});
